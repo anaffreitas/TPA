18ede243160b2edfa6cff77ed60116a37b2a4f76
anaffreitas <anaffreitas0109@gmail.com>
2025-11-17 19:32:40 -0300

    Add files via upload

--- ListaVetores/Somatoria.java
+++ ListaVetores/Somatoria.java
@@ -15,23 +15,23 @@ public class Somatoria {
 		
 		a = new int[TAM];
 		b= new int [TAM];
-		j = 0;
+		j = 0; 
 		
-		for (i=0; i<TAM; i++) {
-			System.out.println("Insira os valores do vetor A: ");
+		System.out.println("Insira os valores do vetor A: ");
+			for (i=0; i<TAM; i++) {
 			a[i] = in.nextInt();
-		}
-		
-		for (i=0; i<TAM; i++) {
-			for (j=0; j< a[i]; j++) {
-				b[i] += a[j];
 			}
+		
+			for (i=0; i<TAM; i++) {
+				for (j=0; j< a[i]; j++) {
+				b[i] += a[j]; // a[j] é o que anda no vetor A, o += soma o vetor A dentro do vetor B
+			} 
 		}
+			
 		System.out.println("Somatoria de A: ");
-		for (i=0; i<TAM; i++) {
-			System.out.print(b[i] + " ");
-	}
+			for (i=0; i<TAM; i++) {
+				System.out.print(b[i] + " ");
+			}
 	
-}
-
+	}
 }
